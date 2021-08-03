f32a3a11c8fff5bdb6650d41a37fd83ee6a74c03
libiao <biaolilibiao_v@didiglobal.com>
2021-08-03 10:18:52 +0800

    init

---
+++ vue/my-test/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3 :class="$style.h3">Ecosystem</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
+
+<style model>
+h3 {
+  margin: 40px 0 0;
+  color: aqua;
+}
+</style>
